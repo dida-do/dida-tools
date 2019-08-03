e69cbf8b6dd46ac6a9ed28903e78e4e9aa37bd1f
mattes <mattesm@zedat.fu-berlin.de>
2019-08-03 16:19:29 +0200

    add directory initializer

--- utils/path.py
+++ utils/path.py
@@ -6,10 +6,14 @@ import os
 from os import path
 import shutil
 
-def make_dir_if_missing(directory: str):
+def create_dirs(directory: str):
     if not path.exists(directory):
         os.makedirs(directory)
 
-def remove_if_exists(path: str):
+def remove_dirs(path: str):
     if path.exists(path):
         shutil.rmtree(path)
+
+def init_directories(config: dict):
+    for val in config.values():
+        create_dirs(val)
