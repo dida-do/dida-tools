c966287472917a1a962c51600e6bc874818aead7
mattes <mattes.mollenhauer@dida.do>
2019-08-22 13:14:05 +0000

    add module docstring

--- train.py
+++ train.py
@@ -1,5 +1,26 @@
 """
-Contains a generic training interface
+Contains a template command line interface for training.
+
+As an example, the training.fastai_training template routine is used
+in combination with the models.unet.UNET model.
+
+The data loading is provided by a generic .npy array wrapper
+in utils.datasets.NpyDataset, transformations and augmentation can be
+added easily in the corresponding class.
+
+We assume that the data is split into test and training examples
+in the paths
+<data_root>/train/x
+<data_root>/train/y
+<data_root>/test/x
+<data_root>/test/y
+where "x" corresponds to observation features and "y" corresponds to labels.
+We additionally assume that in "x" and "y", the correspondence
+is provided by files carrying the same file name.
+
+We do NOT provide a full Argparse suite, since every training routine
+needs its own arguments provided by an external training config file/dictionary.
+An example config is provided in training.fastai_training.
 """
 
 import argparse
@@ -11,8 +32,7 @@ from models.unet import UNET
 from training.pytorch import train
 from utils.path import init_directories
 
-def cli():
-    #TODO
+def cli(train_config):
     pass
 
 if __name__ == "__main__":
