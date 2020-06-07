82bfb97ec531c11aa952df50e939c35702529b37
Matthias Werner <matthias.werner@dida.do>
2020-06-07 13:35:29 +0000

    refactored test_masked_losses.py

--- tests/test_losses/test_masked_losses.py
+++ tests/test_losses/test_masked_losses.py
@@ -6,7 +6,7 @@ import numpy as np
 import torch
 from utils.loss import masked_precision, masked_recall, masked_f1, masked_smooth_dice_loss
 
-class TestMaskedLoss(unittest.TestCase):
+class TestMeasure(unittest.TestCase):
     """
     Base class for testing losses that consider the usability mask. It provides dummy
     images with varying similarity, such that various degrees of the loss functions / metrics
@@ -37,65 +37,91 @@ class TestMaskedLoss(unittest.TestCase):
             self.negative_seg_mask_logits = self.negative_seg_mask_logits.cuda()
             self.half_negative_logits = self.half_negative_logits.cuda()
 
-class TestMaskedPrecision(TestMaskedLoss):
+    def _test_worstcase(self):
+        loss = self.measure(self.seg_mask_logits[:, 1:], self.negative_seg_mask_logits)
+        assert np.isclose(loss.item(), self.worstcase_target, atol=self.worstcase_tol)
+
+    def _test_bestcase(self):
+        loss = self.measure(self.seg_mask_logits[:, 1:], self.seg_mask_logits)
+        assert np.isclose(loss.item(), self.bestcase_target, atol=self.bestcase_tol)
+
+    def _test_order(self):
+        negative = self.measure(self.seg_mask_logits[:, 1:], self.negative_seg_mask_logits)
+        similar = self.measure(self.seg_mask_logits[:, 1:], self.half_negative_logits)
+        equal = self.measure(self.seg_mask_logits[:, 1:], self.seg_mask_logits)
+
+        if self.higher_is_better:
+            assert negative < similar < equal
+        else:
+            assert negative > similar > equal
+
+class TestMaskedPrecision(TestMeasure):
+    measure = staticmethod(masked_precision)
+    bestcase_target = 1.
+    bestcase_tol = 1e-6
+    worstcase_target = 0.
+    worstcase_tol = 1e-6
+    higher_is_better = True
+
     def test_worstcase(self):
-        loss = masked_precision(self.seg_mask_logits[:, 1:], self.negative_seg_mask_logits)
-        assert loss == 0.
+        self._test_worstcase()
 
     def test_bestcase(self):
-        loss = masked_precision(self.seg_mask_logits[:, 1:], self.seg_mask_logits)
-        assert np.isclose(loss.item(), 1., atol=1e-6)
+        self._test_bestcase()
 
     def test_order(self):
-        small_loss = masked_precision(self.seg_mask_logits[:, 1:], self.negative_seg_mask_logits)
-        medium_loss = masked_precision(self.seg_mask_logits[:, 1:], self.half_negative_logits)
-        large_loss = masked_precision(self.seg_mask_logits[:, 1:], self.seg_mask_logits)
-        assert large_loss > medium_loss > small_loss
+        self._test_order()
 
-class TestMaskedRecall(TestMaskedLoss):
+class TestMaskedRecall(TestMeasure):
+    measure = staticmethod(masked_recall)
+    bestcase_target = 1.
+    bestcase_tol = 1e-6
+    worstcase_target = 0.
+    worstcase_tol = 1e-6
+    higher_is_better = True
+
     def test_worstcase(self):
-        loss = masked_recall(self.seg_mask_logits[:, 1:], self.negative_seg_mask_logits)
-        assert loss == 0.
+        self._test_worstcase()
 
     def test_bestcase(self):
-        loss = masked_recall(self.seg_mask_logits[:, 1:], self.seg_mask_logits)
-        assert np.isclose(loss.item(), 1., atol=1e-6)
+        self._test_bestcase()
 
     def test_order(self):
-        small_loss = masked_recall(self.seg_mask_logits[:, 1:], self.negative_seg_mask_logits)
-        medium_loss = masked_recall(self.seg_mask_logits[:, 1:], self.half_negative_logits)
-        large_loss = masked_recall(self.seg_mask_logits[:, 1:], self.seg_mask_logits)
-        assert large_loss > medium_loss > small_loss
+        self._test_order()
 
-class TestMaskedF1Score(TestMaskedLoss):
+class TestMaskedF1Score(TestMeasure):
+    measure = staticmethod(masked_f1)
+    bestcase_target = 1.
+    bestcase_tol = 1e-6
+    worstcase_target = 0.
+    worstcase_tol = 1e-6
+    higher_is_better = True
+
     def test_worstcase(self):
-        loss = masked_f1(self.seg_mask_logits[:, 1:], self.negative_seg_mask_logits)
-        assert loss == 0.
+        self._test_worstcase()
 
     def test_bestcase(self):
-        loss = masked_f1(self.seg_mask_logits[:, 1:], self.seg_mask_logits)
-        assert np.isclose(loss.item(), 1., atol=1e-6)
+        self._test_bestcase()
 
     def test_order(self):
-        small_loss = masked_f1(self.seg_mask_logits[:, 1:], self.negative_seg_mask_logits)
-        medium_loss = masked_f1(self.seg_mask_logits[:, 1:], self.half_negative_logits)
-        large_loss = masked_f1(self.seg_mask_logits[:, 1:], self.seg_mask_logits)
-        assert large_loss > medium_loss > small_loss
+        self._test_order()
 
-class TestSmoothDiceLoss(TestMaskedLoss):
+class TestSmoothDiceLoss(TestMeasure):
+    measure = staticmethod(masked_smooth_dice_loss)
+    bestcase_target = 0.
+    bestcase_tol = 1e-2
+    worstcase_target = 1.
+    worstcase_tol = 1e-2
+    higher_is_better = False
+
     def test_worstcase(self):
-        loss = masked_smooth_dice_loss(self.seg_mask_logits[:, 1:], self.negative_seg_mask_logits)
-        assert np.isclose(loss.item(), 1., atol=1e-2)
+        self._test_worstcase()
 
     def test_bestcase(self):
-        loss = masked_smooth_dice_loss(self.seg_mask_logits[:, 1:], self.seg_mask_logits)
-        assert np.isclose(loss.item(), 0., atol=1e-2)
+        self._test_bestcase()
 
     def test_order(self):
-        small_loss = masked_smooth_dice_loss(self.seg_mask_logits[:, 1:], self.seg_mask_logits)
-        medium_loss = masked_smooth_dice_loss(self.seg_mask_logits[:, 1:], self.half_negative_logits)
-        large_loss = masked_smooth_dice_loss(self.seg_mask_logits[:, 1:], self.negative_seg_mask_logits)
-        assert large_loss > medium_loss > small_loss
+        self._test_order()
 
 if __name__ == "__main__":
     unittest.main()
